8c15d1e72ea3d8b16a432533a2ff763ded98a0cf
Bapu114 <gtmaster114@gmail.com>
2026-02-17 16:30:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BACKEND_URL="https://suthar-ai-frontend.onrender.com";
+const BACKEND_URL="https://suthar-ai01.onrender.com";
 let prompts={
 classical:"Redesign bungalow into luxury classical villa, ultra realistic, 4k render",
 modern:"Redesign bungalow into modern luxury contemporary villa, ultra realistic, 4k render",
